1f7cfdf0067a6ab328ffaf911cc9431592da3113
cal6541 <44925698+i-am-cal@users.noreply.github.com>
2021-09-05 12:02:00 +0100

    Example mod.

---
+++ MainMod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MelonLoader;
+using UnityEngine;
+
+namespace BigscreenExampleMod
+{
+    public class MainMod : MelonMod
+    {
+        public override void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                MelonLogger.Log("Woah, thats cool. You just pressed the letter T on your keyboard!");
+            }
+        }
+    }
+}
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,10 +1,13 @@
-﻿using System.Reflection;
+﻿using BigscreenExampleMod;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using MelonLoader;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// MELONLOADER STUFF
+[assembly: MelonInfo(typeof(MainMod), "Bigscreen Example Mod", "1.0.0", "cal6541")]
+
+// Ignore this
 [assembly: AssemblyTitle("BigscreenExampleMod")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -13,24 +16,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f3b745ac-c15a-4f49-b577-ba0867fbd384")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
