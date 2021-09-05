b7124c4187850d58faacf7521abf3d691129f7d3
cal6541 <44925698+i-am-cal@users.noreply.github.com>
2021-09-05 12:09:15 +0100

    Comments

--- MainMod.cs
+++ MainMod.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using HarmonyLib;
 using MelonLoader;
 using UnityEngine;
 
@@ -6,11 +7,23 @@ namespace BigscreenExampleMod
 {
     public class MainMod : MelonMod
     {
+        // Equiv of MonoBehaviour OnUpdate()
         public override void OnUpdate()
         {
             if (Input.GetKeyDown(KeyCode.T))
             {
                 MelonLogger.Log("Woah, thats cool. You just pressed the letter T on your keyboard!");
+            }
+        }
+
+        // A harmony patch.
+        // This patch targets Assets.Scripts.AppInitializer - It will add a little bit of our code into the method.
+        [HarmonyPatch(typeof(Assets.Scripts.AppInitializer), "Initialize")]
+        class MyHarmonyPatch
+        {
+            public static void Patch()
+            {
+                MelonLogger.Log("If you are reading this! Hello from a Harmony Patch @ Assets.Scripts.AppInitializer!");
             }
         }
     }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,22 +1,8 @@
 ﻿using BigscreenExampleMod;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using MelonLoader;
 
-// MELONLOADER STUFF
+// typeof(MainMod) - The main class of your mod.
+// "Bigscreen Example Mod" - The title of the mod.
+// "1.0.0" - The version of the mod.
+// "cal6541" - The author.
 [assembly: MelonInfo(typeof(MainMod), "Bigscreen Example Mod", "1.0.0", "cal6541")]
-
-// Ignore this
-[assembly: AssemblyTitle("BigscreenExampleMod")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BigscreenExampleMod")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
-[assembly: Guid("f3b745ac-c15a-4f49-b577-ba0867fbd384")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
